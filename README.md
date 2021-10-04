2fe89a4dc2371a8eff90de03da26239838814493
Irfanbd <gourob83@gmail.com>
2021-10-05 01:09:04 +0600

    Basic

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BoiPoka_personal_library
- It will be a basic entry website for my personal library
+
+It will be a basic entry website for my personal library
+
+Database name=crud
+
+table name=boipoka
+bid
+bname
+wname
+genre
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+$sql = "SELECT * FROM 'boipoka' WHERE ";
+$result = mysqli_query($conn, $sql) or die("Unsuccessful Query");
+if (mysqli_num_rows($result) > 0) {
+    ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,9 +15,37 @@
     <title>Boi Poka</title>
 </head>
 <body>
+
     <div class="container">
 <H1>Welcome To My World</H1>
 
+<table class="table table-striped">
+<thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>
+    <a href="edit.php">Edit</a>
+    <a href="delete.php">Delete</a>
+    </td>
+    </tr>
+    
+    
+  </tbody>
+</table>
+<?php
+}
+?>
     </div>
 </body>
 </html>
