d83a937a988596c72ce46eb7d317b2c8885b4e14
Irfanbd <gourob83@gmail.com>
2021-10-08 01:14:20 +0600

    wrong

--- add.php
+++ add.php
@@ -1,28 +1,40 @@
 <?php include 'header.php'; ?>
+
 <div id="main-content">
     <h2>Add New Record</h2>
     <form class="post-form" action="savedata.php" method="post">
+
+    
         <div class="form-group">
             <label>Name</label>
-            <input type="text" name="sname" />
+            <input type="text" name="bname" />
         </div>
         <div class="form-group">
             <label>Address</label>
-            <input type="text" name="saddress" />
+            <input type="text" name="wname" />
         </div>
         <div class="form-group">
-            <label>Class</label>
-            <select name="class">
-                <option value="" selected disabled>Select Class</option>
+            <label>Genre</label>
+            <select name="genre">
+                <option value="" selected disabled>Select your book genre</option>
+
+                <?php
+$conn=mysqli_connect("localhost","root","","crud") or die("Connection Faild");
+$sql="SELECT* FROM boipoka";
+$result=mysqli_query($conn,$sql) or die("Unsuccessful Query");
+
+while(%row=mysqli_fetch_assoc($result))
+{
+
+    <option value="1">BCA</option>
+}
+    ?>
                 <option value="1">BCA</option>
                 <option value="2">BSC</option>
                 <option value="3">B.TECH</option>
             </select>
         </div>
-        <div class="form-group">
-            <label>Phone</label>
-            <input type="text" name="sphone" />
-        </div>
+       
         <input class="submit" type="submit" value="Save"  />
     </form>
 </div>
---
+++ savedata.php
@@ -0,0 +1,5 @@
+<?php
+echo $b_name=$_post['bname'];
+$w_name=$_post['wnamr'];
+$genre=$_post['genre'];
+?>
