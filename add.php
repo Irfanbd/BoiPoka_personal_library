bfe799591ee21cc19071ead5240a5cb305e20a4e
Irfanbd <gourob83@gmail.com>
2021-10-08 10:55:48 +0600

    Error
    Data insert error fixed

--- add.php
+++ add.php
@@ -2,6 +2,13 @@
 
 <div id="main-content">
     <h2>Add New Record</h2>
+    <?php
+$conn=mysqli_connect("localhost","root","","crud") or die("Connection Faild");
+$sql="SELECT* FROM boipoka";
+$result=mysqli_query($conn,$sql) or die("Unsuccessful Query");
+if(mysqli_num_rows($result)>0){
+
+    ?>
     <form class="post-form" action="savedata.php" method="post">
 
     
@@ -14,29 +21,17 @@
             <input type="text" name="wname" />
         </div>
         <div class="form-group">
-            <label>Genre</label>
-            <select name="genre">
-                <option value="" selected disabled>Select your book genre</option>
-
-                <?php
-$conn=mysqli_connect("localhost","root","","crud") or die("Connection Faild");
-$sql="SELECT* FROM boipoka";
-$result=mysqli_query($conn,$sql) or die("Unsuccessful Query");
-
-while(%row=mysqli_fetch_assoc($result))
-{
-
-    <option value="1">BCA</option>
-}
-    ?>
-                <option value="1">BCA</option>
-                <option value="2">BSC</option>
-                <option value="3">B.TECH</option>
-            </select>
+            <label>Book Genre</label>
+            <input type="text" name="genre" />
         </div>
-       
+        
         <input class="submit" type="submit" value="Save"  />
     </form>
+ <?php   
+} 
+?>
+
+ 
 </div>
 </div>
 </body>
--- savedata.php
+++ savedata.php
@@ -1,5 +1,10 @@
 <?php
-echo $b_name=$_post['bname'];
-$w_name=$_post['wnamr'];
-$genre=$_post['genre'];
+$b_name=$_POST['bname'];
+$w_name=$_POST['wname'];
+$genre=$_POST['genre'];
+
+$conn=mysqli_connect("localhost","root","","crud") or die("Connection Faild");
+$sql="INSERT INTO boipoka(bname,wname,genre)VALUES('{$b_name}','{$w_name}','{$genre}')";
+$result=mysqli_query($conn,$sql) or die("Unsuccessful Query");
+header("Location:http://localhost/boipoka_personal_library/index.php")
 ?>
